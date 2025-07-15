1c5738a3595a02976e92abaddc0dda26a7afa274
Gilbert Mutai <mutaigilly02@gmail.com>
2025-07-15 22:45:59 +0300

    Added a bool to check the type of layout whether it's partition and replaced the row and column checks.

--- VVibrantDynamicLayout/VVibrantDynamicLayout.Client/Configuration/Layout/LayoutComponent.cs
+++ VVibrantDynamicLayout/VVibrantDynamicLayout.Client/Configuration/Layout/LayoutComponent.cs
@@ -7,5 +7,6 @@ public class LayoutComponent
     public string Type { get; set; } = string.Empty;
     public string Width { get; set; } = string.Empty;
     public string Alignment { get; set; } = string.Empty;
+    public bool IsPartition { get; set; }
     public List<LayoutComponent> Components { get; set; } = default!;
 }
