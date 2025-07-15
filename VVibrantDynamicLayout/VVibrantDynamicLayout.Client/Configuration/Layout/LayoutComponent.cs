7cb5a7a0cd0c194ba45d377b05d605e3fa7695b5
Gilbert Mutai <mutaigilly02@gmail.com>
2025-07-15 21:38:50 +0300

    A fully responsive dynamic prototype layout

--- VVibrantDynamicLayout/VVibrantDynamicLayout.Client/Configuration/Layout/LayoutComponent.cs
+++ VVibrantDynamicLayout/VVibrantDynamicLayout.Client/Configuration/Layout/LayoutComponent.cs
@@ -4,8 +4,7 @@ namespace VVibrantDynamicLayout.Client.Configuration;
 public class LayoutComponent
 {
     public string Id { get; set; } = string.Empty;
-    //[JsonConverter(typeof(JsonStringEnumConverter))]
-    public string Type { get; set; }
+    public string Type { get; set; } = string.Empty;
     public string Width { get; set; } = string.Empty;
     public string Alignment { get; set; } = string.Empty;
     public List<LayoutComponent> Components { get; set; } = default!;
